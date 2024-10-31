7ff47f90584654ae290c2cf8365c1e70144fe76a
unknown <Bawa Ernest Kwame>
2024-10-31 23:19:29 +0000

    stop watch

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+import Stopwatch from './Stopwatch'
+
+
+const App = () => {
+  return (
+    <div>
+  <h1>  
+    <Stopwatch/>
+
+  </h1>
+      
+    </div>
+  )
+}
+
+export default App
